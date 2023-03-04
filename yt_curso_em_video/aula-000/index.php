a41fb44491b32dcf5effd13b98747b21b465acb5
gabrieldeori <deori.pessoal@gmail.com>
2023-03-03 23:40:50 -0300

    feat: echo/print delimitadores, tags

--- yt_curso_em_video/aula-000/index.php
+++ yt_curso_em_video/aula-000/index.php
@@ -4,16 +4,57 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../../src/dark_theme/prism.css">
+  <link rel="stylesheet" href="../../src/dark_theme/style.css">
   <title>Me livrando da maldição</title>
 </head>
 <body>
   <h1>
     <?php
-
+      echo "Hello, world! \u{1F30E}";
     ?>
   </h1>
-  <p>Livrai-me da maldição</p>
+  <p>Livrai-me da maldição.</p>
+  <p>
+    <h2>Paturso</h2>
+    <?php
+      print "print \"E aí, print ou echo?\"<br>";
+      echo "echo \"Preferiria enfrentar o pato do tamanho de um urso ou urso do tamanho de um pato?\"<br><br>";
+    ?>
+    <span class="atenção">Atenção: Existe diferença entre usar '', "" ou ``</span>
+    <ul>
+      <li>
+        'Aspas simples': Quando usadas para delimitar uma string em um comando echo, o PHP não interpreta nenhuma variável ou caractere especial dentro da string. Tudo dentro das aspas simples é considerado como uma string literal. Por exemplo:
+        <pre>
+          <code class="language-php">
+            $nome = "João";
+            echo 'Olá, $nome!'; // imprime: Olá, $nome!
+          </code>
+        </pre>
+      </li>
+      <li>
+        Aspas duplas ("") - Quando usadas para delimitar uma string em um comando echo, o PHP interpreta variáveis e caracteres especiais, como as sequências de escape \n para nova linha e \t para tabulação. Por exemplo:
+        <pre>
+          <code class="language-php">
+            $nome = "João";
+            echo "Olá, $nome!"; // imprime: Olá, João!
+          </code>
+        </pre>
+      </li>
+      <li>
+        Acento grave ou crase (``) - Quando usados para delimitar uma string em um comando echo, o PHP interpreta variáveis, caracteres especiais e permite a inclusão de expressões PHP dentro da string, usando a sintaxe ${expressão}. Por exemplo:
+        <pre>
+          <code class="language-php">
+            $valor = 100;
+            echo "O valor é: ${valor} reais"; // imprime: O valor é: 100 reais
+          </code>
+        </pre>
+      </li>
+    </ul>
+  </p>
+  <p>
+    Em resumo, as aspas simples são úteis quando você tem uma string literal e não precisa de nenhuma interpretação ou substituição de variáveis. As aspas duplas são úteis quando você precisa incluir variáveis em uma string. Já o acento grave é útil quando você precisa incluir variáveis e expressões PHP em uma string.
+  </p>
   <p>
     PHP já teve duas formas:
   </p>
@@ -24,25 +65,31 @@
       <th>uso</th>
     </tr>
     <tr>
-      <td><code>&lt;?php ?&gt;</code></td>
+      <td><code class="language-php">&lt;?php ?&gt;</code></td>
       <td>Super tag</td>
       <td>Padrão atual</td>
     </tr>
     <tr>
-      <td><code>&lt;? ?&gt;</code></td>
-      <td>Short tag</td>
-      <td>Não recomendado - Funciona</td>
+      <td><code class="language-php">&lt;?= ?&gt;</code></td>
+      <td>Short tag ?</td>
+      <td>Existe habilitada - ?</td>
     </tr>
-      <td><code>&lt;% %&gt;</code></td>
+    <tr>
+      <td><code class="language-php">&lt;? ?&gt;</code></td>
+      <td>Short tag</td>
+      <td>Normalmente desabilitado - Funciona</td>
+    </tr>
+      <td><code class="language-php">&lt;% %&gt;</code></td>
       <td>ASP tag</td>
       <td>Legado - Não funciona</td>
     </tr>
     <tr>
-      <td><code>&lt;script language="php"&gt;&lt;/script&gt;'</code></td>
+      <td><code class="language-php">&lt;script language="php"&gt;&lt;/script&gt;'</code></td>
       <td>?</td>
       <td>Legado - Não funciona</td>
     </tr>
     <tr>
   </table>
+  <script src="../../src/dark_theme/prism.js"></script>
 </body>
 </html>
