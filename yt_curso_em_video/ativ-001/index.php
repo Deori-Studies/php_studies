5dd8472a1a0efc45ab0fa9cf389191ad627d1638
gabrieldeori <deori.pessoal@gmail.com>
2023-03-04 00:02:05 -0300

    chore: identação body index

--- yt_curso_em_video/ativ-001/index.php
+++ yt_curso_em_video/ativ-001/index.php
@@ -6,10 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
-<body>
-  <h1>Dados do Servidor</h1>
-  <?php
-    phpinfo();
-  ?>
-</body>
+  <body>
+    <h1>Dados do Servidor</h1>
+    <?php
+      phpinfo();
+    ?>
+  </body>
 </html>
