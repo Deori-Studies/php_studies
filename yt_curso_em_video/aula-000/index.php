d0338b52ae7c6f6630b01d36cfbf0922438c8b16
gabrieldeori <deori.pessoal@gmail.com>
2023-03-03 22:40:46 -0300

    feat: iniciando com tabela de tags

---
+++ yt_curso_em_video/aula-000/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Me livrando da maldição</title>
+</head>
+<body>
+  <h1>
+    <?php
+
+    ?>
+  </h1>
+  <p>Livrai-me da maldição</p>
+  <p>
+    PHP já teve duas formas:
+  </p>
+  <table>
+    <tr>
+      <th>tags</th>
+      <th>nome</th>
+      <th>uso</th>
+    </tr>
+    <tr>
+      <td><code>&lt;?php ?&gt;</code></td>
+      <td>Super tag</td>
+      <td>Padrão atual</td>
+    </tr>
+    <tr>
+      <td><code>&lt;? ?&gt;</code></td>
+      <td>Short tag</td>
+      <td>Não recomendado - Funciona</td>
+    </tr>
+      <td><code>&lt;% %&gt;</code></td>
+      <td>ASP tag</td>
+      <td>Legado - Não funciona</td>
+    </tr>
+    <tr>
+      <td><code>&lt;script language="php"&gt;&lt;/script&gt;'</code></td>
+      <td>?</td>
+      <td>Legado - Não funciona</td>
+    </tr>
+    <tr>
+  </table>
+</body>
+</html>
