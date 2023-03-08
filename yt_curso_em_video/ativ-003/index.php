d55b2f3de8ab2f455234e03a01759fbe90ad3414
gabrieldeori <deori.pessoal@gmail.com>
2023-03-08 11:27:23 -0300

    refact: envolvendo conteúdos em article

--- yt_curso_em_video/ativ-003/index.php
+++ yt_curso_em_video/ativ-003/index.php
@@ -13,20 +13,22 @@
 </head>
 <body>
   <h1>Configuração básica do php no php.ini</h1>
-  <p>
-    Básicos:<br>
-    O servidor deve ser reiniciado em toda alteração de configurações no servidor.<br>
-    Sempre consulte a documentação no próprio php.ini, nem sempre é bom ligar as configurações.<br>
-  </p>
-  <ul>
-    <li>display_startup_errors = On/Off: Exibir erros de interpretação.</li>
-      display_startup_errors é recomendado ser desligado quando em produção.<br>
-    <li>short_open_tag = On/Off: Processa as short open tags</li>
-      No caso das short_open_tags tenha cuidado já que se o servidor não processar essas tags
-      ele vai enviar todo o conteúdo não processado para o cliente tornando o código visível.
-      Isso pode ser desastroso dependendo do conteúdo e contexto de segurança.<br>
-      O problema de ligar é que talvez o servidor para o qual vai enviar ou migrar o código
-      pode não permitir alterações no php.ini. Pode gerar dores de cabeças.
-  </ul>
+  <article>
+    <h2>Básicos:</h2>
+    <p>
+      O servidor deve ser reiniciado em toda alteração de configurações no servidor.<br>
+      Sempre consulte a documentação no próprio php.ini, nem sempre é bom ligar as configurações.<br>
+    </p>
+    <ul>
+      <li>display_startup_errors = On/Off: Exibir erros de interpretação.</li>
+        display_startup_errors é recomendado ser desligado quando em produção.<br>
+      <li>short_open_tag = On/Off: Processa as short open tags</li>
+        No caso das short_open_tags tenha cuidado já que se o servidor não processar essas tags
+        ele vai enviar todo o conteúdo não processado para o cliente tornando o código visível.
+        Isso pode ser desastroso dependendo do conteúdo e contexto de segurança.<br>
+        O problema de ligar é que talvez o servidor para o qual vai enviar ou migrar o código
+        pode não permitir alterações no php.ini. Pode gerar dores de cabeças.
+    </ul>
+  </article>
 </body>
 </html>
