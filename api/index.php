1c23805e3fccbec0b7b46ea264bb0abcdb161027
gabrieldeori <deori.pessoal@gmail.com>
2023-03-08 13:13:15 -0300

    deploy: Configurando diretórios do deploy

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./src/style/reset.css">
+  <link rel="stylesheet" href="./src/util/prism/prism.css">
+  <link rel="stylesheet" href="./src/style/base.css">
+  <link rel="stylesheet" href="./src/style/style.css">
+  <link rel="stylesheet" href="./src/style/responsive.css">
+  <title>Início</title>
+</head>
+<body>
+  <header>
+    <h1>Link para os estudos</h1>
+  </header>
+  <main>
+    <nav>
+      <a href="./yt_curso_em_video/ativ-000/index.php">
+        <button>Site Aprendizados - Curso</button>
+      </a>
+    </nav>
+  </main>
+</body>
+</html>
