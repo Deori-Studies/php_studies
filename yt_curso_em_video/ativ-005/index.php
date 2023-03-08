01460137c8c0a10dd0f634662f49babcc107411b
gabrieldeori <deori.pessoal@gmail.com>
2023-03-08 11:51:28 -0300

    refact: Ajustando html 005 para responsivo

--- yt_curso_em_video/ativ-005/index.php
+++ yt_curso_em_video/ativ-005/index.php
@@ -12,123 +12,136 @@
   <title>Tipos primitivos</title>
 </head>
 <body>
-  <h1>Tipos primitidos do PHP</h1>
-  <h2>Tipagem</h2>
-  <ul>
-    <li>Dinamicamente tipada.</li>
-    <li>Fracamente tipada.</li>
-  </ul>
-  <h2>Categorias dos tipos primitivos</h2>
-    <h2>Escalares</h2>
-    <ul>
-      <li>$sobrenome = "Silva" | string</li>
-      <li>$idade = 27 | int ou integer</li>
-      <li>$peso = 72.2 | float, double - tipo real só até 7.4</li>
-      <li>$casado = true | bool ou boolean</li>
-    </ul>
-    <h3>Observe que:</h3>
-    <ul>
-      <li>0x1A | Hexadeciaml | INT</li>
-      <li>02818 | Octal | INT</li>
-      <li>0b1010 | Binário | INT</li>
-      <li>3e2 | 3*10^2 | DOUBLE</li>
-    </ul>
+  <header>
+    <h1>Tipos primitidos do PHP</h1>
+  </header>
 
-    <h3>Como declarar: </h3>
-      <pre>
-      <code class="language-php">
-      $string_set = "String Set";
-      $binary_number = 0b1010;
-      $octal_number = 010;
-      $decimal_number = 255;
-      $hex_number = 0x1f;
-      $pow_number = 3e2;
+  <main>
+    <article>
+      <h2>Tipagem</h2>
+      <ul>
+        <li>Dinamicamente tipada.</li>
+        <li>Fracamente tipada.</li>
+      </ul>
+    </article>
+    <article>
+        <h2>Escalares</h2>
+        <ul>
+          <li>$sobrenome = "Silva" | string</li>
+          <li>$idade = 27 | int ou integer</li>
+          <li>$peso = 72.2 | float, double - tipo real só até 7.4</li>
+          <li>$casado = true | bool ou boolean</li>
+        </ul>
+        <section>
+          <h3>Observe que:</h3>
+          <ul>
+            <li>0x1A | Hexadeciaml | INT</li>
+            <li>02818 | Octal | INT</li>
+            <li>0b1010 | Binário | INT</li>
+            <li>3e2 | 3*10^2 | DOUBLE</li>
+          </ul>
+        </section>
 
-      // Outras formas
-      $float_number = (float) "950";
-      $float_number = (double) "950";
-      // Removida na 7.4 VV
-      $float_number = (real) "950";
-      // Removida na 7.4 ^^
-      
-      $string_set = (string) "250";
+        <section>
+          <h3>Como declarar: </h3>
+            <pre>
+            <code class="language-php">
+            $string_set = "String Set";
+            $binary_number = 0b1010;
+            $octal_number = 010;
+            $decimal_number = 255;
+            $hex_number = 0x1f;
+            $pow_number = 3e2;
+            // Outras formas
+            $float_number = (float) "950";
+            $float_number = (double) "950";
+            // Removida na 7.4 VV
+            $float_number = (real) "950";
+            // Removida na 7.4 ^^
+            $string_set = (string) "250";
+            var_dump($string_set) // Mostra informações da variável
+            </code>
+          </pre>
+          <p>Resultados: </p>
+          <ul class="code-result">
+            <?php
+              $string_set = "String Set";
+              $binary_number = 0b1010;
+              $octal_number = 010;
+              $decimal_number = 255;
+              $hex_number = 0x1f;
+              $pow_number = 3e2;
+              echo "<li>";
+              var_dump($string_set);
+              echo "</li>";
+              echo "<li>";
+              var_dump($binary_number);
+              echo "</li>";
+              echo "<li>";
+              var_dump($octal_number);
+              echo "</li>";
+              echo "<li>";
+              var_dump($decimal_number);
+              echo "</li>";
+              echo "<li>";
+              var_dump($hex_number);
+              echo "</li>";
+              echo "<li>";
+              var_dump($pow_number);
+              echo "</li>";
+            ?>
+          </ul>
+        </section>
 
-      var_dump($string_set) // Mostra informações da variável
-      </code>
-    </pre>
-    <p>Resultados: </p>
-    <ul>
-      <?php
-        $string_set = "String Set";
-        $binary_number = 0b1010;
-        $octal_number = 010;
-        $decimal_number = 255;
-        $hex_number = 0x1f;
-        $pow_number = 3e2;
+      <section>
+        <h3>Curiosidades</h3>
+        <p>
+          Em PHP o valor booleano "true = 1" por padrão ou pode ser qualquer valor;<br>
+          Já o valor booleano "false = vazio" por padrão ou pode ser 0, null;<br>
+        </p>
+        <h2>Compostos</h2>
+        <ul>
+          <li>array</li>
+          <li>object</li>
+        </ul>
+        <pre>
+          <code class="language-php">
+            $vet = [6, 2, 9, 3, 5];
+            # echo "O vetor é $vet" // Array to string conversion não existe
+            var_dump($vet);
+            class pessoa { // Classe de objeto
+              private string $nome;
+            }
+            $pessoa_1 = new pessoa; // Nova instância
+            var_dump($pessoa_1)
+          </code>
+        </pre>
+        <p class="code-result">
+          <?php
+            $vet = [6, 2.5, "Maria", 3, false];
+            # echo "O vetor é $vet" // Array to string conversion não existe
+            var_dump($vet);
+            echo "<br><br>";
+            class pessoa { // Classe de objeto
+              private string $nome;
+            }
+            $pessoa_1 = new pessoa; // Nova instância
+            var_dump($pessoa_1)
+          ?>
+        </p>
+      </section>
+    </article>
 
-        echo "<li>";
-        var_dump($string_set);
-        echo "</li>";
-        echo "<li>";
-        var_dump($binary_number);
-        echo "</li>";
-        echo "<li>";
-        var_dump($octal_number);
-        echo "</li>";
-        echo "<li>";
-        var_dump($decimal_number);
-        echo "</li>";
-        echo "<li>";
-        var_dump($hex_number);
-        echo "</li>";
-        echo "<li>";
-        var_dump($pow_number);
-        echo "</li>";
-      ?>
-    </ul>
-  <h3>Curiosidades</h3>
-  <p>
-    Em PHP o valor booleano "true = 1" por padrão ou pode ser qualquer valor;<br>
-    Já o valor booleano "false = vazio" por padrão ou pode ser 0, null;<br>
-  </p>
-  <h2>Compostos</h2>
-  <ul>
-    <li>array</li>
-    <li>object</li>
-  </ul>
-  <pre>
-    <code class="language-php">
-      $vet = [6, 2, 9, 3, 5];
-      # echo "O vetor é $vet" // Array to string conversion não existe
-      var_dump($vet);
-
-      class pessoa { // Classe de objeto
-        private string $nome;
-      }
-      $pessoa_1 = new pessoa; // Nova instância
-      var_dump($pessoa_1)
-    </code>
-  </pre>
-  <p>
-    <?php
-      $vet = [6, 2.5, "Maria", 3, false];
-      # echo "O vetor é $vet" // Array to string conversion não existe
-      var_dump($vet);
-      echo "<br><br>";
-      class pessoa { // Classe de objeto
-        private string $nome;
-      }
-      $pessoa_1 = new pessoa; // Nova instância
-      var_dump($pessoa_1)
-    ?>
-  </p>
-  <h2>Especiais</h2>
-  <ul>
-    <li>null</li>
-    <li>resource</li>
-    <li>callable</li>
-    <li>mixed</li>
-  </ul>
+    <article>
+      <h2>Especiais</h2>
+      <ul>
+        <li>null</li>
+        <li>resource</li>
+        <li>callable</li>
+        <li>mixed</li>
+      </ul>
+    </article>
+  </main>
   <script src="../../src/util/prism/prism.js"></script>
 </body>
 </html>
