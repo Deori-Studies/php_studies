e24946ca353c169cbb276d63c80aad74a8d70856
gabrieldeori <deori.pessoal@gmail.com>
2023-03-08 21:12:31 -0300

    feat: Manipulação de números

--- betabit_curso/index.php
+++ betabit_curso/index.php
@@ -439,7 +439,7 @@
           </code>
         </pre>
         <p>Resultado:</p>
-        <p>
+        <p class="code-result">
           <?php
             $frase = "Hello World x Olá Mundo!";
 
@@ -597,6 +597,140 @@
           <li>callable</li>
           <li>mixed</li>
         </ul>
+      </section>
+    </article>
+
+    <article>
+      <h2>Manipulação de números</h2>
+      <section>
+        <h3>Operadores aritméticos</h3>
+        <ul>
+          <li>+$a identidade | Conversão de $a para int ou float conforme apropriado.</li>
+          <li>-$a negação | Oposto de $a.</li>
+          <li>$a + $b adição | 	Soma de $a e $b.</li>
+          <li>$a - $b subtração | 	Diferença entre $a e $b.</li>
+          <li>$a * $b multiplicação | Produto de $a e $b.</li>
+          <li>$a ** $b exponenciação | Resultado de $a elevado a $b. APENAS PHP 5.6.0 ou maior</li>
+          <li>$a / $b divisão | 	Quociente de $a e $b.</li>
+          <li>$a % $b módulo | 	Resto de $a dividido por $b | Mesmo sinal de $a</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Curiosidade sobre concatenação e operadores aritméticos</h3>
+        <p>
+          O operador + não tem sobrecarga de funções, serve apenas para somar,
+          mas pelo que parece ele converte string numéricas em números, soma
+          e devolve em int ou float.<br>
+          <br>
+          Da versão 7 e anteriores ele aceita somar duas strings não numéricas,
+          ele assume valor 0 para qualquer string não numérica e soma.
+
+          <pre>
+            <code class="language-php">
+              // Versão 7 e anteriores  
+              $result = "101 dálmatas" + "10 dólares";
+              echo $result;
+              // Resultado: int(111);
+              // Porém só entende os primeiros números
+              $result = "101 dálmatas" + "nota 10";
+              echo $result;
+              // Resultado: int(101);
+            </code>
+          </pre>
+        </p>
+        <pre>
+          <code class="language-php">
+            $result = "2" + "2";
+            echo "Usando + &lt;br&gt;";
+            var_dump($result);
+            echo "&lt;br&gt;";
+            $result = "4.1" + "2.3";
+            var_dump($result);
+            echo "&lt;br&gt;";
+            echo "Usando .&lt;br&gt;";
+            $result = "2" . "2";
+            var_dump($result);
+          </code>
+      </pre>
+      <p>Resultado:</p>
+
+      <p class="code-result">
+        <?php
+          $result = "2" + "2";
+          echo "Usando + <br>";
+          var_dump($result);
+          echo "<br>";
+          $result = "4.1" + "2.3";
+          var_dump($result);
+          echo "<br><br>";
+          echo "Usando .<br>";
+          $result = "2" . "2";
+          var_dump($result);
+        ?>
+      </p>
+      </section>
+      <section>
+        <h3>Conversão de valores</h3>
+        <pre>
+          <code class="language-php">
+            $valor = 4 * "2.6";
+            $valor_convertido = (int) $valor;
+            echo "Valor: ";
+            var_dump($valor);
+            echo "&lt;br&gt;Convertido: ";
+            var_dump($valor_convertido);
+          </code>
+        </pre>
+        <p>Resultado:</p>
+        <p class="code-result">
+        <?php
+          $valor = 4 * "2.6";
+          $valor_convertido = (int) $valor;
+          echo "Valor: ";
+          var_dump($valor);
+          echo "<br>Convertido: ";
+          var_dump($valor_convertido);
+        ?>
+        </p>
+      </section>
+
+      <section>
+        <h3>Funções de tipagem</h3>
+        <pre>
+          <code class="language-php">
+            $a = 10;
+            $b = "100";
+            $c = "100a";
+            $d = 2.5;
+
+            var_dump(is_int($a));
+            var_dump(is_int($b));
+            var_dump(is_string($b));
+            var_dump(is_numeric($b));
+            var_dump(is_numeric($c));
+            var_dump(is_string($c));
+            var_dump(is_float($d));
+            var_dump(is_float($b));
+          </code>
+        </pre>
+        <p>Resultado:</p>
+        <p class="code-result">
+          <?php
+            $a = 10;
+            $b = "100";
+            $c = "100a";
+            $d = 2.5;
+
+            var_dump(is_int($a));
+            var_dump(is_int($b));
+            var_dump(is_string($b));
+            var_dump(is_numeric($b));
+            var_dump(is_numeric($c));
+            var_dump(is_string($c));
+            var_dump(is_float($d));
+            var_dump(is_float($b));
+          ?>
+        </p>
       </section>
     </article>
   </main>
