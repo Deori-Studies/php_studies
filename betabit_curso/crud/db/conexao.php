d1dc5d367f2974140e41d18bdcd43a0419b430ef
gabrieldeori <deori.pessoal@gmail.com>
2023-03-10 14:36:48 -0300

    feat: Try Catch ao se conectar com o banco

--- betabit_curso/crud/db/conexao.php
+++ betabit_curso/crud/db/conexao.php
@@ -5,5 +5,11 @@
   $banco = "primeiro_banco";
   $tipo = "mysql";
 
-  $pdo = new PDO("$tipo:host=$servidor;dbname=$banco", $usuario,$senha);
+  try { // Medida de segurança
+    $pdo = new PDO("$tipo:host=$servidor;dbname=$banco", $usuario,$senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  } catch (PDOException $erro) {
+    echo "Falha ao se conectar com o banco!"; # . $erro->getMessage(); // Se quiser...
+  }
 ?>
